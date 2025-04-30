71768821b9430a2eae5178b51c2a878ae71ac65a
choijaewon323 <casdfghjke@naver.com>
2025-04-30 23:21:15 +0900

    fix: 쿼리 빌더 사용 제거

--- src/main/java/com/jaewon/toy/repository/BoardRepositoryCustomImpl.java
+++ src/main/java/com/jaewon/toy/repository/BoardRepositoryCustomImpl.java
@@ -1,38 +1,48 @@
 package com.jaewon.toy.repository;
 
 import com.jaewon.toy.domain.dto.BoardRequiredResponseDto;
-import com.jaewon.toy.util.Query;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.r2dbc.convert.MappingR2dbcConverter;
 import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
-
-import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Repository
 public class BoardRepositoryCustomImpl implements BoardRepositoryCustom {
     private final DatabaseClient databaseClient;
+    private final MappingR2dbcConverter converter;
 
     @Override
     public Flux<BoardRequiredResponseDto> findAllByRequiredColumn() {
-        String sql = Query.builder()
-                .select("b.id as b_id, name, title, nickname, b.created_at as b_created_at, updated_at, like_count")
-                .from("boards as b")
-                .innerJoin("categories as c").on("b.category_id = c.id")
-                .innerJoin("users as u").on("u.id = b.user_id")
-                .build();
+        String sql = """
+                SELECT
+                    b.id AS id,
+                    c.name AS category,
+                    title,
+                    nickname as writer,
+                    b.created_at as created_at,
+                    updated_at,
+                    tmp.cnt AS like_count
+                FROM
+                    boards AS b
+                    INNER JOIN categories AS c
+                        ON b.category_id = c.id
+                    INNER JOIN users AS u
+                        ON u.id = b.user_id
+                    LEFT JOIN (SELECT
+                                    target_id, COUNT(*) as cnt
+                                FROM
+                                    likes
+                                WHERE
+                                    like_type = 'board'
+                                GROUP BY
+                                    target_id) as tmp
+                        ON tmp.target_id = b.id
+                """;
 
         return databaseClient.sql(sql)
-                .map(readable -> BoardRequiredResponseDto.builder()
-                        .id(Long.parseLong(String.valueOf(readable.get("b_id"))))
-                        .title(String.valueOf(readable.get("title")))
-                        .writer(String.valueOf(readable.get("nickname")))
-                        .category(String.valueOf(readable.get("name")))
-                        .createdAt(LocalDateTime.parse(String.valueOf(readable.get("b_created_at"))))
-                        .updatedAt(LocalDateTime.parse(String.valueOf(readable.get("updated_at"))))
-                        .likeCount(Long.parseLong(String.valueOf(readable.get("like_count"))))
-                        .build())
+                .map((row, rowMetadata) -> converter.read(BoardRequiredResponseDto.class, row, rowMetadata))
                 .all();
     }
 }
--- src/main/java/com/jaewon/toy/repository/LikeRepositoryCustomImpl.java
+++ src/main/java/com/jaewon/toy/repository/LikeRepositoryCustomImpl.java
@@ -1,7 +1,6 @@
 package com.jaewon.toy.repository;
 
 import com.jaewon.toy.domain.LikeType;
-import com.jaewon.toy.util.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Repository;
@@ -14,45 +13,59 @@ public class LikeRepositoryCustomImpl implements LikeRepositoryCustom {
 
     @Override
     public Mono<Boolean> deleteByTypeAndTargetId(LikeType likeType, long targetId) {
-        String sql = Query.builder()
-                .deleteFrom("likes")
-                .where("type").eq(likeType.getType())
-                .and("target_id").eq(String.valueOf(targetId))
-                .build();
+        String sql = """
+                DELETE FROM
+                    likes
+                WHERE
+                    like_type = :type AND
+                    target_id = :targetId
+                """;
 
         return databaseClient.sql(sql)
+                .bind("type", likeType.getType())
+                .bind("targetId", targetId)
                 .then()
                 .thenReturn(true);
     }
 
     @Override
     public Mono<Long> countByTypeAndTargetId(LikeType likeType, long targetId) {
-        String sql = Query.builder()
-                .select("count(*)")
-                .from("likes")
-                .where("type").eq("'" + likeType.getType() + "'")
-                .and("target_id").eq(String.valueOf(targetId))
-                .build();
+        String sql = """
+                SELECT
+                    COUNT(*)
+                FROM
+                    likes
+                WHERE
+                    like_type = :type AND
+                    target_id = :targetId
+                """;
 
         return databaseClient.sql(sql)
+                .bind("type", likeType.getType())
+                .bind("targetId", targetId)
                 .map(readable -> Long.parseLong(String.valueOf(readable.get(0))))
                 .one();
     }
 
     @Override
     public Mono<Boolean> isLiked(long userId, long targetId, LikeType likeType) {
-        String sql = Query.builder()
-                .select("count(*)")
-                .from("likes")
-                .where("user_id").eq(String.valueOf(userId))
-                .and("targetId").eq(String.valueOf(targetId))
-                .and("type").eq("'" + likeType.getType() + "'")
-                .build();
+        String sql = """
+                SELECT
+                    COUNT(*)
+                FROM
+                    likes
+                WHERE
+                    user_id = :userId AND
+                    targetId = :targetId AND
+                    like_type = :type
+                """;
 
         return databaseClient.sql(sql)
+                .bind("userId", userId)
+                .bind("targetId", targetId)
+                .bind("type", likeType.getType())
                 .map(readable -> {
                     long count = Long.parseLong(String.valueOf(readable.get(0)));
-
                     return (count > 0);
                 })
                 .one();
@@ -60,14 +73,19 @@ public class LikeRepositoryCustomImpl implements LikeRepositoryCustom {
 
     @Override
     public Mono<Boolean> deleteByTypeAndTargetIdAndUserId(LikeType likeType, long targetId, long userId) {
-        String sql = Query.builder()
-                .deleteFrom("likes")
-                .where("type").eq("'" + likeType.getType() + "'")
-                .and("target_id").eq(String.valueOf(targetId))
-                .and("user_id").eq(String.valueOf(userId))
-                .build();
+        String sql = """
+                DELETE FROM
+                    likes
+                WHERE
+                    like_type = :type AND
+                    target_id = :targetId AND
+                    user_id = :userId
+                """;
 
         return databaseClient.sql(sql)
+                .bind("type", likeType.getType())
+                .bind("target_id", targetId)
+                .bind("user_id", userId)
                 .then()
                 .thenReturn(true);
     }
