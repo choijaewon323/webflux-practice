12a7e477fc57d8a2d419bf5dd5e37fe4fea3a5f2
choijaewon323 <casdfghjke@naver.com>
2025-04-30 22:23:52 +0900

    feat: r2dbc 세팅

---
+++ init/init.sql
@@ -0,0 +1,46 @@
+drop table if exists boards;
+drop table if exists categories;
+drop table if exists likes;
+drop table if exists users;
+drop table if exists replies;
+
+create table boards (
+    id serial primary key,
+    title varchar,
+    content varchar,
+    user_id bigint,
+    like_count bigint,
+    category_id bigint,
+    cnt bigint,
+    created_at timestamp,
+    updated_at timestamp
+);
+
+create table categories (
+    id serial primary key,
+    name varchar
+);
+
+create table likes (
+    id serial primary key,
+    like_type varchar,
+    user_id bigint,
+    target_id bigint
+);
+
+create table users (
+    id serial primary key,
+    nickname varchar,
+    email varchar,
+    password varchar,
+    created_at timestamp
+);
+
+create table replies (
+    id serial primary key,
+    user_id bigint,
+    board_id bigint,
+    content varchar,
+    created_at timestamp,
+    updated_at timestamp
+);
