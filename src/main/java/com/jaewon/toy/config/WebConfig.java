25fd644e17f0e9facb1f6dd65de3804de1023a46
choijaewon323 <casdfghjke@naver.com>
2025-05-01 20:09:49 +0900

    feat: cors 설정 제거

---
+++ src/main/java/com/jaewon/toy/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.jaewon.toy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@EnableWebFlux
+@Configuration
+public class WebConfig implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedMethods("PUT", "DELETE", "GET", "POST");
+    }
+}
