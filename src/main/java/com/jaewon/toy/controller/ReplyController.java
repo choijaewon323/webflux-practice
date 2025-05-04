281197962667874dc03894cb647e069f02ade25f
최재원 <46291115+choijaewon323@users.noreply.github.com>
2025-05-04 22:35:29 +0900

    feat: reply readAll 구현

--- src/main/java/com/jaewon/toy/controller/ReplyController.java
+++ src/main/java/com/jaewon/toy/controller/ReplyController.java
@@ -1,7 +1,9 @@
 package com.jaewon.toy.controller;
 
 import com.jaewon.toy.domain.reply.dto.ReplySaveRequestDto;
+import com.jaewon.toy.service.LogService;
 import com.jaewon.toy.service.ReplyService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +15,25 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api/v1/reply")
 public class ReplyController {
     private final ReplyService replyService;
+    private final LogService logService;
 
     @PostMapping("/{boardId}")
     public Mono<Boolean> save(@PathVariable Long boardId, @RequestBody ReplySaveRequestDto request) {
-        return replyService.save(boardId, request);
+        return replyService.save(boardId, request)
+                .doOnError(logService::saveError)
+                .onErrorReturn(false);
     }
 
     @DeleteMapping("/{replyId}")
     public Mono<Boolean> deleteById(@PathVariable Long replyId) {
-        return replyService.deleteById(replyId);
+        return replyService.deleteById(replyId)
+                .doOnError(logService::saveError)
+                .onErrorReturn(false);
+    }
+
+    @GetMapping("/{boardId}")
+    public Mono<?> getAllByBoardId(@PathVariable Long boardId) {
+        return replyService.getAllRepliesByBoardId(boardId)
+                .doOnError(logService::saveError);
     }
 }
